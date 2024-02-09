132841e371ef28895db40172339c760719089b6c
Vittorio <vittoriohalfon01@gmail.com>
2024-02-09 16:31:51 +0200

    authenticateToken.js wasn't saved

---
+++ server/middlewares/authenticateToken.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Extract token from "Bearer TOKEN"
+
+    if (token == null) return res.sendStatus(401); // If no token, then unauthorized
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403); // Token is no longer valid
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = { authenticateToken };
